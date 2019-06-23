b9e8a44ed82e19e2547fc356e529bcb64038dc0d
maxhha <devmaxhha@gmail.com>
2019-06-23 22:32:37 +0300

    Added multiscreen size support

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,15 +1,16 @@
 extends Control
 
-onready var map = $map
-onready var player = $map/player
+onready var map = $VBoxContainer/Control2/map_control/map
+onready var player = map.get_node("player")
+onready var matchControl = $VBoxContainer/Control/matchControl
 
 func _ready():
-	$matchControl.match_map.connect("swap", self, "_on_swap")
-	map.connect("player_turn", $matchControl, "set_can_swap", [true])
+	matchControl.match_map.connect("swap", self, "_on_swap")
+	map.connect("player_turn", matchControl, "set_can_swap", [true])
 	
 func _on_swap(p1,p2):
 	var dir = p2 - p1
-	$matchControl.can_swap = false
+	matchControl.can_swap = false
 	
 	if map.is_in_room(player.map_pos+dir):
 		map.move(player, player.map_pos+dir)
